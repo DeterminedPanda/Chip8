f231548bdaf54c3da4c786d7391f7a28133a9825
DeterminedPanda <justinas.maniscalco@live.de>
2018-01-25 05:07:56 +0100

    finished 0x7XXX opcodes

--- opcode.c
+++ opcode.c
@@ -36,10 +36,9 @@ void cpu_2(void) {
 }
 
 //TODO name
-//all opcodes that start with 3 are evaluated here
+//Skips the next instruction if VX equals NN.
 void cpu_3(void) {
-	//Skips the next instruction if VX equals NN.
-	unsigned char x = (opcode & 0x0F00) >> 8;
+unsigned char x = (opcode & 0x0F00) >> 8;
 	unsigned char nn = opcode & 0x00FF;
 
 	if(V[x] == nn) {
@@ -50,9 +49,8 @@ void cpu_3(void) {
 }
 
 //TODO name
-//all opcodes that start with 4 are evaluated here
+//Skips the next instruction if VX doesn't equal NN.
 void cpu_4(void) {
-	//Skips the next instruction if VX doesn't equal NN.
 	unsigned char x = (opcode & 0x0F00) >> 8;
 	unsigned char nn = opcode & 0x00FF;
 
@@ -65,9 +63,8 @@ void cpu_4(void) {
 }
 
 //TODO name
-//all opcodes that start with 5 are evaluated here
+//Skips the next instruction if VX equals VY.
 void cpu_5(void) {
-	//Skips the next instruction if VX equals VY.
 	unsigned char x = (opcode & 0x0F00) >> 8;
 	unsigned char y = (opcode & 0x00F0) >> 4;
 
@@ -79,9 +76,12 @@ void cpu_5(void) {
 }
 
 //TODO name
-//all opcodes that start with 6 are evaluated here
+//Sets VX to NN
 void cpu_6(void) {
+	unsigned char x = (opcode & 0x0F00) >> 8;
+	unsigned char nn = opcode & 0x00FF;
 
+	V[x] = nn;
 }
 
 //TODO name
