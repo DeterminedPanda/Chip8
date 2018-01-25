57524ab287652660b925d16da59f0eb6dcb5f5c6
DeterminedPanda <justinas.maniscalco@live.de>
2018-01-25 04:52:02 +0100

    Finished 0x9XXX opcodes

--- opcode.c
+++ opcode.c
@@ -72,56 +72,65 @@ void cpu_arithmetic(void) {
 	unsigned char y = (opcode & 0x00F0) >> 4;
 
 	switch(tail) {
-		case(0x0000):
+		case(0x0000): //Sets VX to the value of VY. 
 			V[x] = V[y];
 			break;
-		case(0x0001):
+		case(0x0001): //Sets VX to VX or VY. 
 			V[x] = V[x] | V[y];
 			break;
-		case(0x0002):
+		case(0x0002): //Sets VX to VX and VY.	
 			V[x] = V[x] & V[y];
 			break;
-		case(0x0003):
+		case(0x0003): //Sets VX to VX xor VY.
 			V[x] = V[x] ^ V[y];
 			break;
-		case(0x0004):
+		case(0x0004): //Adds VY to VX. VF is set to 1 when there's a carry, and to 0 when there isn't.
 			V[x] += V[y];
 			break;
-		case(0x0005):
-			//possible FIXME, should V[x] only be subtracted by V[y]
-			//when V[y] is bigger than V[X]?
+		case(0x0005): //VY is subtracted from VX. VF is set to 0 when there's a borrow, and 1 when there isn't.
 			V[x] -= V[y];
-			if(y > x) {
+			if(V[y] > V[x]) {
 				V[15] = 0;	
 			} else {
 				V[15] = 1;
 			}
 			break;
-		case(0x0006):
+		case(0x0006): //Shifts VY right by one and copies the result to VX. VF is set to the value of the least significant bit of VY before the shift.
 			V[15] = V[y] & 1; //get least significant bit
 			V[x] = V[y] >> 1;
 			break;
-		case(0x0007):
-			//possible FIXME, should V[y] only be subtracted by V[x]
-			//when V[x] is bigger than V[y]?
-			if(x > y) {
+		case(0x0007): //Sets VX to VY minus VX. VF is set to 0 when there's a borrow, and 1 when there isn't.
+			if(V[x] > V[y]) {
 				V[15] = 0;
 			} else {
 				V[15] = 1;
 			}
 			V[x] = V[y] - V[x];
 			break;
-		case(0x000E): 
+		case(0x000E): //Shifts VY left by one and copies the result to VX. VF is set to the value of the most significant bit of VY before the shift 
 			V[15] = (V[y] >> 15) & 1; //get most significant bit
 			V[x] = V[y] >> 1;
 			break;
 	}
+	pc += 2;
 }
 
 //TODO name
 //all opcodes that start with 9 are evaluated here
 void cpu_9(void) {
+	unsigned char tail = opcode & 0x000F;
+	unsigned char x = (opcode & 0x0F00) >> 8;
+	unsigned char y = (opcode & 0x00F0) >> 4;
 
+	switch(tail) {
+		case 0: //Skips the next instruction if VX doesn't equal VY. 
+			if(V[x] != V[y]) {
+				pc += 4;
+			} else {
+				pc+= 2;
+			}
+			break;
+	}
 }
 
 //TODO name
