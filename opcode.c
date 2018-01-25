bca871dd498cebf7617d7aad235aadf7b2184564
DeterminedPanda <justinas.maniscalco@live.de>
2018-01-25 05:00:11 +0100

    finished 0x4XXX opcodes

--- opcode.c
+++ opcode.c
@@ -39,7 +39,7 @@ void cpu_2(void) {
 //all opcodes that start with 3 are evaluated here
 void cpu_3(void) {
 	//Skips the next instruction if VX equals NN.
-	unsigned char x = (opcode & 0x0F00) >> 8;
+unsigned char x = (opcode & 0x0F00) >> 8;
 	unsigned char nn = opcode & 0x00FF;
 
 	if(V[x] == nn) {
@@ -47,11 +47,21 @@ void cpu_3(void) {
 	} else {
 		pc += 2;
 	}
-}
+
+	}
 
 //TODO name
 //all opcodes that start with 4 are evaluated here
 void cpu_4(void) {
+//Skips the next instruction if VX doesn't equal NN.
+unsigned char x = (opcode & 0x0F00) >> 8;
+	unsigned char nn = opcode & 0x00FF;
+
+	if(V[x] != nn) {
+		pc += 4;
+	} else {
+		pc += 2;
+	}
 
 }
 
